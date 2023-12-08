b1ef380436a2777258a0e420818863cbf047885f
Vanessa <vanessa.riosm571@gmail.com>
2023-12-08 13:02:11 -0500

    Feat: add todo controller

---
+++ TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+    }
+}
